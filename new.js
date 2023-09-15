b9c0d68881b1122a7e632944ec79fff112d2c202
AlonaHlobina <54394529+AlonaHlobina@users.noreply.github.com>
2023-09-15 14:27:44 +0200

    Update new.js

--- new.js
+++ new.js
@@ -1,6 +1,8 @@
 document.write(window.location.search);
 /*
-This ^^ causes an alert that won't be reported in the PR because
-it was already in the code.
+
+
+
+
 */
 document.write(window.location.search);
